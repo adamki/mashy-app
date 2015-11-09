ef85b5d02185167412ac171311d8df3bbf5bd848
adamki <adajensen@gmail.com>
2015-11-09 14:22:32 -0700

    other rails generated files

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, "API_KEY", "API_SECRET"
+  provider :twitter, "JbAXWFF8F8yHHjHT1KRRrcyb8", "wKCLIvoBFZygSSjs5dAfYqdyAxlIem7R3PEDJp4T5NQxxCKCCF"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root 'dashboard#index'
+
+  get '/auth/twitter', as: :login
+  get '/auth/twitter/callback' => 'sessions#create'
+  get "/logout" => 'sessions#destroy'
 end
