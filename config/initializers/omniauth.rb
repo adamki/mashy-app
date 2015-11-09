636e982de243d0cefa734fc5b6e55fec18b40765
adamki <adajensen@gmail.com>
2015-11-09 14:41:15 -0700

    changes to omniauth.rb to swap out for ENV variables. new keys generated on app.twitter.com

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, "JbAXWFF8F8yHHjHT1KRRrcyb8", "wKCLIvoBFZygSSjs5dAfYqdyAxlIem7R3PEDJp4T5NQxxCKCCF"
+  provider :twitter, ENV['API_KEY'], ENV['API_SECRET']
 end
