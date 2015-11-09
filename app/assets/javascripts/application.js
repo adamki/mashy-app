5e8963f8fdf1982a019e24bdec77874ba2201727
adamki <adajensen@gmail.com>
2015-11-08 17:44:30 -0700

    beginning general SPA layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,8 @@
 //= require semantic-ui
 //= require materialize-sprockets
 //= require_tree .
+
+// Initialize collapse button
+$(".button-collapse").sideNav();
+// Initialize collapsible (uncomment the line below if you use the dropdown variation)
+//$('.collapsible').collapsible();
