c3a024443784ae455cec16c5e9540738c24b4902
adamki <adajensen@gmail.com>
2015-11-10 18:17:17 -0700

    fixed typo on config/omniauth.rb:4

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 require 'rspotify/oauth'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, ENV["your_client_id"], ENV["your_client_secret", scope: 'user-read-email playlist-modify-public user-library-read user-library-modify']
+  provider :spotify, ENV["your_client_id"], ENV["your_client_secret"], scope: 'user-read-email playlist-modify-public user-library-read user-library-modify']
 end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
