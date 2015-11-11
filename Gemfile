31b174ac92c66289c0c9fda4afbda1fc40e91723
adamki <adajensen@gmail.com>
2015-11-10 18:06:15 -0700

    other needed changes to allow for implentation of SPotify gem and Oauth

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'rspotify'
 gem 'puma'
 
 gem 'semantic-ui-sass', github: 'doabit/semantic-ui-sass'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151109211623) do
+ActiveRecord::Schema.define(version: 20151111010023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20151109211623) do
     t.string   "oauth_token_secret"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+    t.string   "email"
   end
 
 end
