48ac5e688e7474adc9f18ee8f2fddfe1eebda370
adamki <adajensen@gmail.com>
2015-11-15 12:55:01 -0700

    index and show endpoints working for tracks, users, pls

--- app/serializers/track_serializer.rb
+++ app/serializers/track_serializer.rb
@@ -1,3 +1,3 @@
 class TrackSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :spotify_id, :name, :artist, :url, :external_image, :created_at, :updated_at, :href, :owner, :playlist_id
 end
