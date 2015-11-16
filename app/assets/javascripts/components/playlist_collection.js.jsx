1a543dc6f028a67a4c8f3f67c5b7375825136be1
adamki <adajensen@gmail.com>
2015-11-16 11:33:11 -0700

    gettting ready to render echoresults

--- app/assets/javascripts/components/playlist_collection.js.jsx
+++ app/assets/javascripts/components/playlist_collection.js.jsx
@@ -4,7 +4,9 @@ var PlaylistCollection = React.createClass({
                 tracks: [],
               playlist: {},
           showPlaylist: false,
-            showtracks: false };
+            showtracks: false,
+       showEchoResults: false,
+          echoResponse: false};
   },
   componentDidMount: function() {
     $.ajax({
@@ -16,32 +18,50 @@ var PlaylistCollection = React.createClass({
     });
   },
   showPlaylistHandler: function(playlist, event){
-    var spotify_id = playlist.spotify_id
+    var spotify_id = playlist.spotify_id;
     $.ajax({
       url: '/api/v1/playlists/' + spotify_id,
       type: 'GET',
       success: function(response){
-        this.setState({ playlist: response })
-        this.setState({ tracks: response.tracks })
-        this.setState({ showPlaylist: !this.state.showPlaylist })
+        this.setState({ playlist: response });
+        this.setState({ tracks: response.tracks });
+        this.setState({ showPlaylist: !this.state.showPlaylist });
       }.bind(this)
     });
   },
-  getTrackHandler: function(playlist){
+  getEchoHandler: function(playlist){
+    var track_ids = [];
+    playlist.tracks.map(function(track){
+      track_ids.push(track.spotify_id);
+    });
+
+    var that = this;
+    var response = track_ids.map(function(id){
+      $.ajax({
+        url: '/api/v1/tracks/' + id,
+        type: 'GET',
+        success: function(response){
+          that.setState({echoResponse: response});
+        }.bind(this)
+      });
+    });
+    // this.setState.echoResponse={response}
   },
   render: function(){
     return(
       <div className="playlistsBox">
         <h1>Playlists</h1>
         <AllPlaylists playlists={this.state.playlists} getPlaylist={this.showPlaylistHandler}/>
+        {this.state.showEchoResults ? < ResultsDisplay echoResponse={this.state.echoResponse}
+                                    /> : null}
         {this.state.showPlaylist ? <SinglePlaylistBox
+                                        getEchoResults={this.getEchoHandler}
                                               playlist={this.state.playlist}
-                                                tracks={this.state.tracks}
-                                                scores={this.state.scores}
-                                              getTrack={this.getTrackHandler}
+                                              tracks={this.state.tracks}
+                                              scores={this.state.scores}
                                     /> : null}
       </div>
-    )
+    );
   }
 });
 
@@ -67,23 +87,28 @@ var AllPlaylists = React.createClass({
 });
 
 var SinglePlaylistBox = React.createClass({
+  handleClick: function() {
+    this.props.getEchoResults(this.props);
+    return(
+      console.log(this.props.tracks)
+    );
+  },
   render: function(){
     var track_list = this.props.tracks.map(function(track, index){
       return(
-        <li key={track.id}>
-          {track.name} by: {track.artist}
+        <li key={track.id} className="collection-item">
+          {track.name} by: <strong>{track.artist}</strong>
         </li>
       );
     }.bind(this));
-      return(
-        <div className="playlist-details">
-          <h3>{this.props.playlist.name}</h3>
-          <h5>{this.props.playlist.owner}</h5>
-          <ul>
-            {track_list}
-          </ul>
-          <button onClick={this.props.getTrack.bind(null, this.props)}>Analize this Playlist</button>
-        </div>
+    return(
+      <div className="playlist-details">
+        <h3>{this.props.playlist.name} by: {this.props.playlist.owner}</h3>
+        <button onClick={this.handleClick}>Analize this Playlist</button>
+        <ul className="collection with-header" data-collapsible="accordion">
+          {track_list}
+        </ul>
+      </div>
     );
   }
 });
